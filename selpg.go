117837527b1f4c389e45b4ea5cf3b38fdcbee1d0
wtysos11 <wtysos11@163.com>
2018-10-08 18:51:28 +0800

    can read stdin and file

--- selpg.go
+++ selpg.go
@@ -3,17 +3,17 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 import flag "github.com/spf13/pflag"
 
-var startPage = flag.IntP("start","s",-1,"start page")
-var endPage = flag.IntP("end","e",-1,"end page")
-var lineNumber = flag.IntP("line","l",72,"line number in one page")
-var format = flag.BoolP("format","f",false,"whether to find \f and make it as standard")
-var destination = flag.StringP("dest","d","","destination of output")
-
 func main(){
-
+	var startPage = flag.IntP("start","s",-1,"start page")
+	var endPage = flag.IntP("end","e",-1,"end page")
+	var lineNumber = flag.IntP("line","l",72,"line number in one page")
+	var format = flag.BoolP("format","f",false,"whether to find \f and make it as standard")
+	var destination = flag.StringP("dest","d","","destination of output")	
 	flag.Parse()
 //debug
 	fmt.Printf("args=%s, num=%d\n", flag.Args(), flag.NArg())
@@ -25,6 +25,27 @@ func main(){
 	fmt.Println("line=",*lineNumber)
 	fmt.Println("format=",*format)
 	fmt.Println("desination=",*destination)
+
+	var data []byte
+	var err error
+	switch flag.NArg(){
+	case 0:
+		data,err = ioutil.ReadAll(os.Stdin)
+		if(err!=nil){
+			fmt.Println("error:",err)
+			os.Exit(1)
+		}
+	case 1:
+		data,err = ioutil.ReadFile(flag.Arg(0))
+		if(err!=nil){
+			fmt.Println("error:",err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Printf("Input must have one argument as file name or no argument.")
+		os.Exit(1)
+	}
+	fmt.Printf("data is:%s",string(data))
 //legal check
 	//-s and -e must use
 
